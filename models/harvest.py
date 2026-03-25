bf0c0288f03cd92616203070f41928ce8cb243a9
Jehoshaphat Ogbonnia Urom <jehoshaphaturom@gmail.com>
2026-03-25 15:05:24 +0100

    Add more models for logs

--- models/harvest.py
+++ models/harvest.py
@@ -6,11 +6,13 @@ from sqlalchemy import Column, String, ForeignKey, Date, DateTime
 from sqlalchemy.orm import relationship
 
 
-class Monitoring_log(BaseModel, Base):
-    """Representation of monitoring model for eggs development"""
-    __tablename__ = 'monitoring_logs'
-    temp = Column(Float)
-    humidity = Column(Float)
-    larvae_density = Column(Float)
+class Harvest_log(BaseModel, Base):
+    """Representation of harvest model for monitoring
+    larvae development and residue"""
+    __tablename__ = 'harvest_logs'
+    larvae_weight = Column(Float)
+    avg_larvae_size = Column(Float)
+    residue_weight = Column(Float)
+    residue_frass_grade = Column(String)
     timestamp = Column(DateTime, default=datetime.utcnow().time)
     batch_id = Column(String(60), ForeignKey('batches.id'), nullable=False)
